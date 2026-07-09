592e777ea74434a149df637c37e241c1c2ce17d5
Mbeldoz <emaildc17@gmail.com>
2026-07-09 16:32:24 +0700

    Add game list and load script based on PlaceId

--- script.lua
+++ script.lua
@@ -1,1 +1,6 @@
-print("Hello")
+local gameList = {
+ [97598239454123] = "https://nolag-eml.pages.dev/Game/GAG2.lua",
+ [4924922222] = "", -- +1 Speed Keyboard
+}
+local url = gameList[game.PlaceId]
+if url then loadstring(game:HttpGet(url))() end
